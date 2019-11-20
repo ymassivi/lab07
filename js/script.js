1de5b718307a58de21c42930d6c30b030575c261
ymassivi <54606260+ymassivi@users.noreply.github.com>
2019-11-19 22:20:42 -0600

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function init() {
-document.getElementById("entrybutton").addEventListener("click", alertMes);
-function alertMes() {
-var message = document.getElementById("entryinput").value;
-alert("Yannick Massivi: " + message);
-document.getElementById("textoutput").innerHTML= message;
-}
-window.addEventListener("load", init);
